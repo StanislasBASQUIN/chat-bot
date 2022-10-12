4e240ed149a38bd43e45d88471cbcfdddcc0ab4d
BASQUIN <stanislas.basquin@gmail.com>
2022-10-12 16:19:36 +0200

    Second Commit on Dev : Deploy ChatBot

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-# pip install nltk
-# Developpement du ChatBot.
-
 from nltk.chat.util import Chat, reflections
 
 # Redéfinir le dialogue.
+# Définir le dialogue.
+
+# Partie Gauche est la Question de User. Partie Droite est la réponse du Bot.
 pairs = [
     ["Mon Prenom est Ucef", ["Bonjour Ucef"]]
+    ["Mon Prenom est (.*)", ["Bonjour %1"]],
+    ["(Bonjour|Salut|Coucou|Hello)", ["Bonjour", "Hello", "Comment puis-je vous aider?"]],
+    ["Je souhaite acheter un (.*) volume (.*)", ["Daccord, je vais vous préparer les %1 Volume %2"]],
+    ["(.*) (ville|adresse)", ["Paris xxxx"]]
 ]
 
 chat = Chat(pairs, reflections)
-chat.converse()
-
-
