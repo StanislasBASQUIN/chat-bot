db3267d21fe926529756efc6d654d6f5824a2ba9
BASQUIN <stanislas.basquin@gmail.com>
2022-10-12 15:24:42 +0200

    First Commit on Dev

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 # pip install nltk
 # Developpement du ChatBot.
 
+from nltk.chat.util import Chat, reflections
+
+# Redéfinir le dialogue.
+pairs = [
+    ["Mon Prenom est Ucef", ["Bonjour Ucef"]]
+]
+
+chat = Chat(pairs, reflections)
+chat.converse()
+
+
