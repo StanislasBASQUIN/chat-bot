5ca137e2897000c1165d4e70861c0e2c670fa6c6
BASQUIN <stanislas.basquin@gmail.com>
2022-10-12 17:10:02 +0200

    Third Commit on Dev : Adding Main & Graphic

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from nltk.chat.util import Chat, reflections
 
 # Partie Gauche est la Question de User. Partie Droite est la réponse du Bot.
 pairs = [
-    ["Mon Prenom est Ucef", ["Bonjour Ucef"]]
     ["Mon Prenom est (.*)", ["Bonjour %1"]],
     ["(Bonjour|Salut|Coucou|Hello)", ["Bonjour", "Hello", "Comment puis-je vous aider?"]],
     ["Je souhaite acheter un (.*) volume (.*)", ["Daccord, je vais vous préparer les %1 Volume %2"]],
     ["(.*) (ville|adresse)", ["Paris xxxx"]]
 ]
-
 chat = Chat(pairs, reflections)
+chat.converse()
+
+
